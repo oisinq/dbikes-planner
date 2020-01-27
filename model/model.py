a3b1117c9218d28b18bede6a09cc20a89c348012
Oisin Quinn <oisin1001@gmail.com>
2020-01-27 02:05:04 +0000

    Improvements to model, removed unnecessary variables

--- model/model.py
+++ model/model.py
@@ -36,46 +36,46 @@ def display_breakdown():
             plt.title(attributes[i+1])
     plt.show()
 
+def test_model(gam):
+    # time_of_day,type_of_day,day_of_year,temperature,rain,relative_humidity,vapour_pressure,wind_speed,sunshine,visibility
+    # 21: 50625,1,1951425,22,2017-01-22T14:03:45,5.1,0,64,5.6,3,0.7,20000
+    tester = [50625,1,22,5.1,0,64,5.6,3]
+
+    y_pred = gam.predict([tester])
+
+    print(y_pred)
+
+    # 0,75229,0,6036829,69,
+    # tester = [75229,0,69,10.2,0,81,10.0,6,0.0,30000]
+
+    # y_pred = gam.predict([tester])
+
+    # print(y_pred)
+
+    # (38 previous)
+    # 27,29121,0,22838721,264,11.9,0,69,9.6,4,0.0,60000,-11,1474355121,38,40
+
+    tester = [29121,0,264,11.9,0,69,9.6,4]
+
+    y_pred = gam.predict([tester])
+
+    print(y_pred)
+
 my_data = pd.read_csv("result_charlemount.csv")
 
-attributes = ['time_of_day', 'type_of_day', 'day_of_year', 'temperature', 'rain', 'relative_humidity', 'vapour_pressure', 'wind_speed', 'sunshine', 'visibility', 'change_in_bike_availability']
+attributes = ['time_of_day', 'type_of_day', 'day_of_year', 'temperature', 'rain', 'relative_humidity', 'vapour_pressure', 'wind_speed']
+
 X = my_data[attributes].values
+#Xother = my_data[['time_of_day', 'type_of_day', 'day_of_year', 'temperature']].values
+
 y = my_data['available_bike_stands'].values
 
-#display_bike_availability_over_time(my_data)
+gam = LinearGAM(te(0, 1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7), n_splines=[65, 20, 20, 10, 20, 10, 10, 25], dtype=['numerical', 'categorical', 'numerical', 'numerical', 'numerical', 'numerical', 'numerical', 'numerical'])
 
-gam = LinearGAM(te(0, 1) + s(2) + s(3) + s(4) + s(5) + s(6) + s(7) + s(8) + s(9) +s(10), n_splines=[50, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20], dtype=['numerical', 'categorical', 'numerical', 'numerical', 'numerical', 'numerical', 'numerical', 'numerical', 'numerical', 'numerical', 'numerical'])
 gam.gridsearch(X, y)
 
 gam.summary()
 
 display_breakdown()
 
-# time_of_day,type_of_day,day_of_year,temperature,rain,relative_humidity,vapour_pressure,wind_speed,sunshine,visibility
-# 21: 50625,1,1951425,22,2017-01-22T14:03:45,5.1,0,64,5.6,3,0.7,20000
-tester = [50625,1,22,5.1,0,64,5.6,3,0.7,20000,20,20]
-
-y_pred = gam.predict([tester])
-
-print(y_pred)
-
-# 0,75229,0,6036829,69,
-# tester = [75229,0,69,10.2,0,81,10.0,6,0.0,30000]
-
-# y_pred = gam.predict([tester])
-
-# print(y_pred)
-
-# (38 previous)
-# 27,29121,0,22838721,264,11.9,0,69,9.6,4,0.0,60000,-11,1474355121,38,40
-
-tester = [29121,0,264,11.9,0,69,9.6,4,0.0,60000,38,40]
-
-y_pred = gam.predict([tester])
-
-print(y_pred)
-
-
-# plt.plot(predi, c='r', ls='--')
-# plt.plot(y_pred, c='k')
-# plt.show()
+test_model(gam)
