4afaeae371634eeb7a60c3cd2d8f5d1649cc340f
Oisin Quinn <oisin1001@gmail.com>
2020-05-10 19:25:38 +0100

    Removed stations/ folder from webapp

---
+++ data/scripts/normal.py
@@ -0,0 +1,25 @@
+from sklearn.preprocessing import OrdinalEncoder, MinMaxScaler
+import pandas as pd
+
+
+def timestamp_fix():
+    data = pd.read_csv("/Users/oisin/Coding/ucd/Fourth/fyp/fyp/webapp/stations/Charlemont Street.csv")
+
+    X = data.iloc[:, [2, 3, 4, 6, 7, 9, 10, 12, 15]]
+    y = data.iloc[:, [14]]
+
+    print("aight")
+    encoder = OrdinalEncoder(categories=[['empty', 'very low', 'low', 'moderate', 'high']])
+
+    minmax = MinMaxScaler()
+
+    X_scaled = pd.DataFrame(minmax.fit_transform(X), columns=X.columns)
+    y_scaled = pd.DataFrame(encoder.fit_transform(y.values.reshape(-1, 1)), columns=y.columns)
+
+    print(X_scaled)
+    print(y_scaled)
+    #    y = encoder.fit_transform(y.values.reshape(-1, 1))
+    #X = minmax.fit_transform(X)
+
+if __name__ == "__main__":
+    timestamp_fix()
